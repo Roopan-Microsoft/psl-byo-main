4d10d8fa61f9852cdc5733dd360b21e2142bfaa5
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-18 01:22:20 +0530

    install pyodbc

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -24,6 +24,9 @@ sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
 sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
 sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
 
+apt-get update && apt-get install -y build-essential unixodbc-dev g++
+pip install pyodbc
+
 pip install -r requirements.txt
 
 python create_sql_user.py
