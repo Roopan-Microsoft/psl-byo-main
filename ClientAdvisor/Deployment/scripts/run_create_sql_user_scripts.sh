c38a4d7dadaf92e64721a020509aa168bc231096
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-18 17:34:43 +0530

    pip setuptools wheel

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Started the script"
 
-# Install required packages
+# Update package list and install required packages
 echo "Updating package list and installing Python and required packages..."
 sudo apt-get update -y
-sudo apt-get install -y python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
+sudo apt-get install -y python3 python3-dev g++ build-essential unixodbc-dev libpq-dev
 
 # Install pyodbc using pip
+pip3 install --upgrade pip setuptools wheel
 pip3 install pyodbc
 
 echo "Installation complete."
