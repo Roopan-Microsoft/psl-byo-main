d633b2eaa6fcff9ad45e9e23ecebe7f13017cc06
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-18 09:59:58 +0530

    pyodbc

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -19,13 +19,20 @@ curl --output "$requirementFile" "$requirementFileUrl"
 
 echo "Download completed"
 
-#Replace key vault name 
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
-sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
-sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
+# Replace key vault name and other placeholders
+sed -i "s/kv_to-be-replaced/${safeKeyvaultName}/g" "create_sql_user.py"
+sed -i "s/miClientId_to-be-replaced/${safeMiClientId}/g" "create_sql_user.py"
+sed -i "s/user_to-be-replaced/${safeUserName}/g" "create_sql_user.py"
 
-sudo apt-get update && sudo apt-get install -y unixodbc-dev g++
-pip install --upgrade pip
+RUN apt-get update
+RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
+apk add python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
+ 
+# RUN apt-get install python3 python3-dev g++ unixodbc-dev unixodbc libpq-dev
+pip install pyodbc
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Execute Python script
 python create_sql_user.py
