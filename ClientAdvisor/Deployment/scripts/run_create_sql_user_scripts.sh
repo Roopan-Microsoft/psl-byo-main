b56c5e3f5a14f86d0f1a065613cec6f7f13c09c4
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-18 13:50:41 +0530

    error_handler

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -2,6 +2,15 @@
 set -e  # Exit on error
 echo "Started the script"
 
+# Function to handle errors
+error_handler() {
+    echo "An error occurred in the script. Exiting..."
+    exit 1
+}
+
+# Trap any errors and call the error_handler function
+trap 'error_handler' ERR
+
 # Variables
 baseUrl="$1"
 keyvaultName="$2"
@@ -14,17 +23,11 @@ echo "Downloading necessary scripts..."
 
 # Download the create_sql_user Python file
 curl --output "create_sql_user.py" "${baseUrl}Deployment/scripts/index_scripts/create_sql_user.py"
-if [ $? -ne 0 ]; then
-    echo "Error downloading create_sql_user.py from ${baseUrl}."
-    exit 1
-fi
+echo "Downloaded create_sql_user.py"
 
 # Download the requirement file
 curl --output "$requirementFile" "$requirementFileUrl"
-if [ $? -ne 0 ]; then
-    echo "Error downloading requirements.txt from ${baseUrl}."
-    exit 1
-fi
+echo "Downloaded requirements.txt"
 
 echo "Download completed"
 
@@ -37,11 +40,7 @@ userName=$(printf '%s\n' "$userName" | sed 's/[&/\]/\\&/g')
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
 sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
 sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
-
-if [ $? -ne 0 ]; then
-    echo "Error replacing placeholders in create_sql_user.py."
-    exit 1
-fi
+echo "Replaced placeholders in create_sql_user.py"
 
 echo "Installing necessary dependencies..."
 
@@ -54,12 +53,14 @@ if command -v apt-get &> /dev/null; then
                  curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
                  apt-get update && \
                  ACCEPT_EULA=Y apt-get install -y msodbcsql18"
+    echo "ODBC Driver installed."
 elif command -v yum &> /dev/null; then
     echo "Installing ODBC Driver 18 for SQL Server..."
     sudo su -c "yum update -y && \
                  curl https://packages.microsoft.com/keys/microsoft.asc | rpm --import - && \
                  curl https://packages.microsoft.com/config/rhel/$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2)/prod.repo > /etc/yum.repos.d/mssql-release.repo && \
                  ACCEPT_EULA=Y yum install -y msodbcsql18"
+    echo "ODBC Driver installed."
 else
     echo "Unsupported package manager. Please install ODBC Driver manually."
     exit 1
@@ -72,9 +73,10 @@ source myenv/bin/activate
 # Install Python dependencies
 pip install --upgrade pip  # Upgrade pip to the latest version
 pip install -r "$requirementFile"
+echo "Python dependencies installed."
 
 echo "Executing Python script..."
 # Execute the Python script
-#python3 create_sql_user.py
+python3 create_sql_user.py
 
 echo "Script completed successfully."
