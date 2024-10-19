43357b79f4b1797a4e0994d492ce2e9740c3c872
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 12:44:08 +0530

    --prefer-binary

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 echo "Started the script"
 
+# Create a Python virtual environment in /tmp
 python3 -m venv /tmp/myenv
 source /tmp/myenv/bin/activate
 
 # Install necessary system packages
 sudo apt-get update -y
-sudo apt-get install -y build-essential python3-dev unixodbc-dev
+sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
 
 # Upgrade pip, setuptools, and wheel
 pip install --upgrade pip setuptools wheel
 
-# Install pyodbc directly
-pip install pyodbc
+# Install pyodbc and prefer binary wheels
+pip install pyodbc --prefer-binary
 
 echo "Installation complete."
