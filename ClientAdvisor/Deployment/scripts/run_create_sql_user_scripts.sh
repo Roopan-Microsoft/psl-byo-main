2bcd7ae610b1e376e7c0c2796a21859c1c84e9b8
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 12:09:48 +0530

    venv myenv

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "Started the script"
 
+# Set up a Python virtual environment
+if ! python3 -m venv myenv; then
+    echo "Error: Failed to create Python virtual environment."
+    exit 1
+fi
+source myenv/bin/activate
+
 # Make sure all dependencies are installed
 sudo apt-get update -y
 sudo apt-get install -y build-essential python3-dev unixodbc-dev
