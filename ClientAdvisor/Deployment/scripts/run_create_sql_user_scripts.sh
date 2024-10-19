ce9fa9a289668085018d7390c8810ebeead28763
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 12:26:47 +0530

    venv /tmp/myenv

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 echo "Started the script"
 
-# Set up a Python virtual environment
-if ! python3 -m venv myenv; then
-    echo "Error: Failed to create Python virtual environment."
-    exit 1
-fi
-source myenv/bin/activate
+python3 -m venv /tmp/myenv
+source /tmp/myenv/bin/activate
 
-# Make sure all dependencies are installed
+# Install necessary system packages
 sudo apt-get update -y
 sudo apt-get install -y build-essential python3-dev unixodbc-dev
 
 # Upgrade pip, setuptools, and wheel
-pip3 install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
 
 # Install pyodbc directly
-pip3 install pyodbc
-
+pip install pyodbc
 
 echo "Installation complete."
