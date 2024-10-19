da7c59c8b32f728add3f5045ddc08f7aa8e1a32f
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 18:32:33 +0530

    without sudo

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -29,18 +29,19 @@ sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
 sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
 sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
 cat create_sql_user.py
+
 # Create a Python virtual environment in /tmp
 python3 -m venv /tmp/myenv
 source /tmp/myenv/bin/activate
 
-# Install necessary system packages
-sudo apt-get update -y
-sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
+# Install necessary system packages (No sudo)
+apt-get update -y
+apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
 
 # Install pyodbc using apt
-sudo apt-get install -y python3-pyodbc
+apt-get install -y python3-pyodbc
 
-echo "Installation complete."
+echo "System dependencies installation complete."
 
 # Install Python dependencies
 echo "Installing Python dependencies from requirements.txt..."
@@ -50,10 +51,10 @@ pip install -r requirements.txt
 echo "Executing create_sql_user.py..."
 python3 create_sql_user.py
 
-# # Check for errors during Python script execution
-# if [ $? -eq 0 ]; then
-#     echo "SQL user creation script executed successfully!"
-# else
-#     echo "Error executing SQL user creation script." >&2
-#     exit 1
-# fi
+# Check for errors during Python script execution
+if [ $? -eq 0 ]; then
+    echo "SQL user creation script executed successfully!"
+else
+    echo "Error executing SQL user creation script." >&2
+    exit 1
+fi
