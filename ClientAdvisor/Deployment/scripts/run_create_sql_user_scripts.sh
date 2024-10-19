7d3f089f4ecbe7fd6b3d1e6f4024faa64b946a59
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 13:37:03 +0530

    echo create_sql_user.py

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -28,32 +28,32 @@ echo "Download completed"
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
 sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
 sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
+echo create_sql_user.py
+# # Create a Python virtual environment in /tmp
+# python3 -m venv /tmp/myenv
+# source /tmp/myenv/bin/activate
 
-# Create a Python virtual environment in /tmp
-python3 -m venv /tmp/myenv
-source /tmp/myenv/bin/activate
+# # Install necessary system packages
+# sudo apt-get update -y
+# sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
 
-# Install necessary system packages
-sudo apt-get update -y
-sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
+# # Install pyodbc using apt
+# sudo apt-get install -y python3-pyodbc
 
-# Install pyodbc using apt
-sudo apt-get install -y python3-pyodbc
+# echo "Installation complete."
 
-echo "Installation complete."
+# # Install Python dependencies
+# echo "Installing Python dependencies from requirements.txt..."
+# pip install -r requirements.txt
 
-# Install Python dependencies
-echo "Installing Python dependencies from requirements.txt..."
-pip install -r requirements.txt
+# # Execute the Python script to create the SQL user
+# echo "Executing create_sql_user.py..."
+# python3 create_sql_user.py
 
-# Execute the Python script to create the SQL user
-echo "Executing create_sql_user.py..."
-python3 create_sql_user.py
-
-# Check for errors during Python script execution
-if [ $? -eq 0 ]; then
-    echo "SQL user creation script executed successfully!"
-else
-    echo "Error executing SQL user creation script." >&2
-    exit 1
-fi
+# # Check for errors during Python script execution
+# if [ $? -eq 0 ]; then
+#     echo "SQL user creation script executed successfully!"
+# else
+#     echo "Error executing SQL user creation script." >&2
+#     exit 1
+# fi
