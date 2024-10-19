25873b8b07c447765dd2d7f5210c07471eb4c1f2
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-19 17:37:29 +0530

    apt-get update

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -29,18 +29,18 @@ sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_sql_user.py"
 sed -i "s/miClientId_to-be-replaced/${miClientId}/g" "create_sql_user.py"
 sed -i "s/user_to-be-replaced/${userName}/g" "create_sql_user.py"
 cat create_sql_user.py
-# # Create a Python virtual environment in /tmp
-# python3 -m venv /tmp/myenv
-# source /tmp/myenv/bin/activate
+# Create a Python virtual environment in /tmp
+python3 -m venv /tmp/myenv
+source /tmp/myenv/bin/activate
 
-# # Install necessary system packages
-# sudo apt-get update -y
-# sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
+# Install necessary system packages
+sudo apt-get update -y
+sudo apt-get install -y build-essential python3-dev unixodbc unixodbc-dev
 
-# # Install pyodbc using apt
-# sudo apt-get install -y python3-pyodbc
+# Install pyodbc using apt
+sudo apt-get install -y python3-pyodbc
 
-# echo "Installation complete."
+echo "Installation complete."
 
 # # Install Python dependencies
 # echo "Installing Python dependencies from requirements.txt..."
