8d16e430416732570d73daa1688987e9721f9b39
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-20 20:02:31 +0530

    user_name

--- ClientAdvisor/Deployment/scripts/index_scripts/create_sql_user.py
+++ ClientAdvisor/Deployment/scripts/index_scripts/create_sql_user.py
@@ -13,30 +13,28 @@ def get_secrets_from_kv(kv_name, secret_name):
     secret_client = SecretClient(vault_url=f"https://{kv_name}.vault.azure.net/", credential=credential)
     return secret_client.get_secret(secret_name).value
 
-try:
-    # Retrieve server and database secrets from Key Vault
-    server = get_secrets_from_kv(key_vault_name, "SQLDB-SERVER")
-    database = get_secrets_from_kv(key_vault_name, "SQLDB-DATABASE")
-    print(f"Server: {server}, Database: {database}")  # Debug info
+# try:
+# Retrieve server and database secrets from Key Vault
+server = get_secrets_from_kv(key_vault_name, "SQLDB-SERVER")
+database = get_secrets_from_kv(key_vault_name, "SQLDB-DATABASE")
+print(f"Server: {server}, Database: {database}")  # Debug info
+# Managed Identity-based authentication
+authentication = 'ActiveDirectoryMsi'
 
-    # Managed Identity-based authentication
-    authentication = 'ActiveDirectoryMsi'
-    
-    # Connection string for SQL Server using Managed Identity
-    conn_string = (
-        f'Driver={{ODBC Driver 18 for SQL Server}};'
-        f'Server={server},1433;'
-        f'Database={database};'
-        f'UID={managed_identity_client_id};'
-        f'Authentication={authentication};'
-        f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    )
-
-    # Establish the connection
-    conn = pyodbc.connect(conn_string)
-    print("Connection established successfully")  # Debug info
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+# Connection string for SQL Server using Managed Identity
+conn_string = (
+    f'Driver={{ODBC Driver 18 for SQL Server}};'
+    f'Server={server},1433;'
+    f'Database={database};'
+    f'UID={managed_identity_client_id};'
+    f'Authentication={authentication};'
+    f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+)
+# Establish the connection
+conn = pyodbc.connect(conn_string)
+print("Connection established successfully")  # Debug info
+# except Exception as e:
+#     print(f"An error occurred: {str(e)}")
 
 cursor = conn.cursor()
 
