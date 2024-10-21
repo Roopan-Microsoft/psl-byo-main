2ae77964b0e6d24e94e27c970dcb4442a9f95dd5
Pavan Kumar <v-kupavan.microsoft.com>
2024-10-22 01:23:32 +0530

    import pyodbc

--- ClientAdvisor/Deployment/scripts/index_scripts/create_sql_user.py
+++ ClientAdvisor/Deployment/scripts/index_scripts/create_sql_user.py
@@ -13,31 +13,32 @@ def get_secrets_from_kv(kv_name, secret_name):
     secret_client = SecretClient(vault_url=f"https://{kv_name}.vault.azure.net/", credential=credential)
     return secret_client.get_secret(secret_name).value
 
-# try:
-# Retrieve server and database secrets from Key Vault
-server = get_secrets_from_kv(key_vault_name, "SQLDB-SERVER")
-database = get_secrets_from_kv(key_vault_name, "SQLDB-DATABASE")
-print(f"Server: {server}, Database: {database}")  # Debug info
-# Managed Identity-based authentication
-authentication = 'ActiveDirectoryMsi'
-managed_identity_client_id='92f4c3bc-0970-4319-ab47-13ed3becb3ea'
+try:
+    import pyodbc
+    # Retrieve server and database secrets from Key Vault
+    server = get_secrets_from_kv(key_vault_name, "SQLDB-SERVER")
+    database = get_secrets_from_kv(key_vault_name, "SQLDB-DATABASE")
+    print(f"Server: {server}, Database: {database}")  # Debug info
+    # Managed Identity-based authentication
+    authentication = 'ActiveDirectoryMsi'
+    managed_identity_client_id='92f4c3bc-0970-4319-ab47-13ed3becb3ea'
 
-# Connection string for SQL Server using Managed Identity
-conn_string = (
-    f'Driver={{ODBC Driver 17 for SQL Server}};'
-    f'Server={server},1433;'
-    f'Database={database};'
-    f'UID={managed_identity_client_id};'
-    f'Authentication={authentication};'
-    f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-)
-print(f"conn_string: {conn_string}")
-print(f"Server: {server}, Database: {database}")
-# # Establish the connection
-# conn = pyodbc.connect(conn_string)
-# print("Connection established successfully")  # Debug info
-# # except Exception as e:
-# #     print(f"An error occurred: {str(e)}")
+    # Connection string for SQL Server using Managed Identity
+    conn_string = (
+        f'Driver={{ODBC Driver 17 for SQL Server}};'
+        f'Server={server},1433;'
+        f'Database={database};'
+        f'UID={managed_identity_client_id};'
+        f'Authentication={authentication};'
+        f'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    )
+    print(f"conn_string: {conn_string}")
+    print(f"Server: {server}, Database: {database}")
+    # Establish the connection
+    conn = pyodbc.connect(conn_string)
+    print("Connection established successfully")  # Debug info
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
 
 # cursor = conn.cursor()
 
