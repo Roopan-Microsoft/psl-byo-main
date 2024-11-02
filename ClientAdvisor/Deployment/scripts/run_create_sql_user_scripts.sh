ce54e2ebddfd8f482d4a42266ee8ba64d0ac0d02
Pavan Kumar <v-kupavan.microsoft.com>
2024-11-02 13:05:00 +0530

    mssql-tools_17

--- ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
+++ ClientAdvisor/Deployment/scripts/run_create_sql_user_scripts.sh
@@ -33,25 +33,17 @@ cat create_sql_user.py
 
 # Install system dependencies for pyodbc
 echo "Installing system packages..."
-apk update
-apk add gcc libc-dev g++ libffi-dev libxml2 unixodbc-dev unixodbc mariadb-dev libstdc++6
-apk add bash icu-libs krb5-libs libgcc libintl libssl1.1 libstdc++ zlib curl gnupg
+apk add --no-cache --virtual .build-deps \
+    build-base \
+    unixodbc-dev
 
 #Download the desired package(s)
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.8.1.1-1_amd64.apk
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.8.1.1-1_amd64.apk
-
-#(Optional) Verify signature, if 'gpg' is missing install it using 'apk add gnupg':
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.8.1.1-1_amd64.sig
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.8.1.1-1_amd64.sig
-
-curl https://packages.microsoft.com/keys/microsoft.asc  | gpg --import -
-gpg --verify msodbcsql17_17.8.1.1-1_amd64.sig msodbcsql17_17.8.1.1-1_amd64.apk
-gpg --verify mssql-tools_17.8.1.1-1_amd64.sig mssql-tools_17.8.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.10.5.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.10.1.1-1_amd64.apk
 
 #Install the package(s)
-apk add --allow-untrusted msodbcsql17_17.8.1.1-1_amd64.apk
-apk add --allow-untrusted mssql-tools_17.8.1.1-1_amd64.apk
+apk add --allow-untrusted msodbcsql17_17.10.5.1-1_amd64.apk
+apk add --allow-untrusted mssql-tools_17.10.1.1-1_amd64.apk
 
 # Install Python dependencies from requirements.txt
 echo "Installing Python dependencies from requirements.txt..."
