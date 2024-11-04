00afd0f794cad33024468d257be69640a862adae
Pavan Kumar <v-kupavan.microsoft.com>
2024-11-04 23:31:16 +0530

    pyodbc test

--- ClientAdvisor/Deployment/scripts/create_azure_functions.sh
+++ ClientAdvisor/Deployment/scripts/create_azure_functions.sh
@@ -32,7 +32,7 @@ az storage account create --name $storageAccount --location eastus --resource-gr
 az functionapp create --resource-group $resourceGroupName --name $functionappname \
                 --environment $env_name --storage-account $storageAccount \
                 --functions-version 4 --runtime python \
-                --image bycwacontainerreg.azurecr.io/byc-wa-fn:$functionAppVersion
+                --image byocgacontainerregpk.azurecr.io/byc-wa-fn:$functionAppVersion
 
 # Sleep for 120 seconds
 echo "Waiting for 120 seconds to ensure the Function App is properly created..."
